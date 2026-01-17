d14079b6246b33d50c8b7cf638df6277361f9cd7
Brunellitech <mateusbrunelli05@gmail.com>
2026-01-16 22:41:07 -0300

    Backend pronto pra deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import cors from 'cors';
 import urlRoutes from './routes/url.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor rodando");
+});
 
 // Middlewares
 app.use(cors());
@@ -15,6 +18,4 @@ app.get('/teste', (req, res) => res.send('Servidor rodando!'));
 // Conecta as rotas
 app.use('/', urlRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+
